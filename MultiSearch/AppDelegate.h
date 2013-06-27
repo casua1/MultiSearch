4ac6501172b7096ec9737fccc3d44b98c6d3efe7
casua1 <hasnolife74@gmail.com>
2013-06-27 18:35:14 -0400

    Initial Commit

---
+++ MultiSearch/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultiSearch
+//
+//  Created by iD Student on 6/27/13.
+//  Copyright (c) 2013 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MultiSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultiSearch
+//
+//  Created by iD Student on 6/27/13.
+//  Copyright (c) 2013 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
